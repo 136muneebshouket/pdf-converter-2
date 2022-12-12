93259b6510f4493a96abc1ccf4847939d5c90a3d
136muneebshouket <92539069+136muneebshouket@users.noreply.github.com>
2022-12-12 20:20:09 +0500

    updated

--- index.js
+++ index.js
@@ -11,14 +11,15 @@ const toPdf = require('mso-pdf');
 const PDFGenerator = require('pdfkit');
 const imagesToPdf = require("images-to-pdf");
 // const rimraf=require('rimraf')
-var findRemoveSync = require('find-remove');
+// var findRemoveSync = require('find-remove');
 const app = express();
 
 
 
 app.use(express.json({ limit: '50mb' }));
+app.use(cors());
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(cors())
+
 
 
 
