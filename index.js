5c60bf27d9fed636bdf13f84da7e17676dbcbcf8
136muneebshouket <92539069+136muneebshouket@users.noreply.github.com>
2022-12-14 17:28:46 +0500

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 const cors = require('cors')
 
 const corsOptions ={
-  origin:'https://precious-malasada-e76702.netlify.app', 
+  origin:'https://office-to-pdf.adaptable.app', 
   credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200
 }
