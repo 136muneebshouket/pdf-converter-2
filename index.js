b0f801733021ca7add056f500938e3546d4cd7e3
136muneebshouket <92539069+136muneebshouket@users.noreply.github.com>
2023-01-08 20:04:18 +0500

    cors changes

--- index.js
+++ index.js
@@ -22,7 +22,11 @@ const cors = require('cors')
 //   credentials:true,            //access-control-allow-credentials:true
 //   optionSuccessStatus:200
 // }
-app.use(cors());
+app.use(cors(
+  {
+    origin:"http://localhost:3000"
+  }
+));
 
 
 app.use(express.json({ limit: '50mb' }));
